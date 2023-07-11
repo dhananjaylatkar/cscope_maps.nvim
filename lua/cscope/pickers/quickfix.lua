abdfd10d2a8a11b5a20147b3a0464374ee597e19
Dhananjay <dhananjay.latkar@gmail.com>
2023-07-11 14:12:54 +0530

    cscope: quickfix use botright

--- lua/cscope/pickers/quickfix.lua
+++ lua/cscope/pickers/quickfix.lua
@@ -3,7 +3,7 @@ local M = {}
 M.run = function(opts)
 	vim.fn.setqflist(opts.cscope.parsed_output, "r")
 	vim.fn.setqflist({}, "a", { title = opts.cscope.prompt_title })
-	vim.api.nvim_command("copen 5")
+	vim.cmd("botright copen 5")
 end
 
 return M

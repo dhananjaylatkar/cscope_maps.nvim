fab01d896713d3cf414a8f2ee7ce73d13293fded
Dhananjay <dhananjay.latkar@gmail.com>
2023-07-11 12:16:01 +0530

    cscope_maps: simplify keymaps function

--- lua/utils/maps_helper.lua
+++ lua/utils/maps_helper.lua
@@ -1,4 +1,5 @@
 local M = {}
+local map = vim.keymap.set
 
 M.ver = vim.version()
 
@@ -41,7 +42,6 @@ M.sym_map = {
 	a = "Find places where this symbol is assigned a value",
 	b = "Build database",
 }
-M.keymap_opts = { noremap = true, silent = true }
 
 M.get_cscope_prompt_cmd = function(operation, selection)
 	local sel = "cword" -- word under cursor
@@ -56,51 +56,28 @@ M.get_cscope_prompt_cmd = function(operation, selection)
 	)
 end
 
-M.keymap_wo_wk = function()
-	-- Without which-key
-	vim.api.nvim_set_keymap("n", "<leader>cs", M.get_cscope_prompt_cmd("s", "w"), M.keymap_opts)
-	vim.api.nvim_set_keymap("n", "<leader>cg", M.get_cscope_prompt_cmd("g", "w"), M.keymap_opts)
-	vim.api.nvim_set_keymap("n", "<leader>cc", M.get_cscope_prompt_cmd("c", "w"), M.keymap_opts)
-	vim.api.nvim_set_keymap("n", "<leader>ct", M.get_cscope_prompt_cmd("t", "w"), M.keymap_opts)
-	vim.api.nvim_set_keymap("n", "<leader>ce", M.get_cscope_prompt_cmd("e", "w"), M.keymap_opts)
-	vim.api.nvim_set_keymap("n", "<leader>cf", M.get_cscope_prompt_cmd("f", "f"), M.keymap_opts)
-	vim.api.nvim_set_keymap("n", "<leader>ci", M.get_cscope_prompt_cmd("i", "f"), M.keymap_opts)
-	vim.api.nvim_set_keymap("n", "<leader>cd", M.get_cscope_prompt_cmd("d", "w"), M.keymap_opts)
-	vim.api.nvim_set_keymap("n", "<leader>ca", M.get_cscope_prompt_cmd("a", "w"), M.keymap_opts)
-	vim.api.nvim_set_keymap("n", "<leader>cb", "<cmd>Cscope build<cr>", M.keymap_opts)
-end
-
-M.keymap_w_wk = function(wk)
-	-- With which-key
-	wk.register({
-		["<leader>"] = {
-			c = {
-				name = "+cscope",
-				s = { M.get_cscope_prompt_cmd("s", "w"), M.sym_map.s },
-				g = { M.get_cscope_prompt_cmd("g", "w"), M.sym_map.g },
-				c = { M.get_cscope_prompt_cmd("c", "w"), M.sym_map.c },
-				t = { M.get_cscope_prompt_cmd("t", "w"), M.sym_map.t },
-				e = { M.get_cscope_prompt_cmd("e", "w"), M.sym_map.e },
-				f = { M.get_cscope_prompt_cmd("f", "f"), M.sym_map.f },
-				i = { M.get_cscope_prompt_cmd("i", "f"), M.sym_map.i },
-				d = { M.get_cscope_prompt_cmd("d", "w"), M.sym_map.d },
-				a = { M.get_cscope_prompt_cmd("a", "w"), M.sym_map.a },
-				b = { "<cmd>Cscope build<cr>", M.sym_map.b },
-			},
-		},
-	}, M.keymap_opts)
+M.default_keymaps = function()
+	local sym_map = M.sym_map
+	local ok, wk = pcall(require, "which-key")
+	if ok then
+		wk.register({["<leader>c"] = {name="+cscope"}}, { noremap = true, silent = true })
+	end
+	map("n", "<leader>cs", M.get_cscope_prompt_cmd("s", "w"), { noremap = true, silent = true , desc=sym_map.s })
+	map("n", "<leader>cg", M.get_cscope_prompt_cmd("g", "w"), { noremap = true, silent = true , desc=sym_map.g })
+	map("n", "<leader>cc", M.get_cscope_prompt_cmd("c", "w"), { noremap = true, silent = true , desc=sym_map.c })
+	map("n", "<leader>ct", M.get_cscope_prompt_cmd("t", "w"), { noremap = true, silent = true , desc=sym_map.t })
+	map("n", "<leader>ce", M.get_cscope_prompt_cmd("e", "w"), { noremap = true, silent = true , desc=sym_map.e })
+	map("n", "<leader>cf", M.get_cscope_prompt_cmd("f", "f"), { noremap = true, silent = true , desc=sym_map.f })
+	map("n", "<leader>ci", M.get_cscope_prompt_cmd("i", "f"), { noremap = true, silent = true , desc=sym_map.i })
+	map("n", "<leader>cd", M.get_cscope_prompt_cmd("d", "w"), { noremap = true, silent = true , desc=sym_map.d })
+	map("n", "<leader>ca", M.get_cscope_prompt_cmd("a", "w"), { noremap = true, silent = true , desc=sym_map.a })
+	map("n", "<leader>cb", "<cmd>Cscope build<cr>", { noremap = true, silent = true , desc=sym_map.b })
 end
 
 M.init_keymaps = function()
-	vim.api.nvim_set_keymap("n", "<C-]>", [[<cmd>exe "Cstag" expand("<cword>")<cr>]], M.keymap_opts)
+	map("n", "<C-]>", [[<cmd>exe "Cstag" expand("<cword>")<cr>]], {noremap = true, silent = true , desc="ctag"})
 
-	local ok, wk = pcall(require, "which-key")
-	if not ok then
-		M.keymap_wo_wk()
-	else
-		-- which-key mappings
-		M.keymap_w_wk(wk)
-	end
+	M.default_keymaps()
 end
 
 return M

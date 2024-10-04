49cfff7a544dde5aecaf69e267b0e3ac9406e26e
Dhananjay <dhananjay.latkar@gmail.com>
2024-10-04 15:41:11 +0530

    cscope: use visually selected text as default sym (#57)

--- lua/cscope_maps/utils/helper.lua
+++ lua/cscope_maps/utils/helper.lua
@@ -29,17 +29,17 @@ M.default_keymaps = function(prefix)
 			end
 		end
 	end
-	map("n", prefix .. "s", "<cmd>CsPrompt s<cr>", { desc = sym_map.s })
-	map("n", prefix .. "g", "<cmd>CsPrompt g<cr>", { desc = sym_map.g })
-	map("n", prefix .. "c", "<cmd>CsPrompt c<cr>", { desc = sym_map.c })
-	map("n", prefix .. "t", "<cmd>CsPrompt t<cr>", { desc = sym_map.t })
-	map("n", prefix .. "e", "<cmd>CsPrompt e<cr>", { desc = sym_map.e })
-	map("n", prefix .. "f", "<cmd>CsPrompt f<cr>", { desc = sym_map.f })
-	map("n", prefix .. "i", "<cmd>CsPrompt i<cr>", { desc = sym_map.i })
-	map("n", prefix .. "d", "<cmd>CsPrompt d<cr>", { desc = sym_map.d })
-	map("n", prefix .. "a", "<cmd>CsPrompt a<cr>", { desc = sym_map.a })
-	map("n", prefix .. "b", "<cmd>Cs db build<cr>", { desc = sym_map.b })
-	map("n", "<C-]>", "<cmd>Cstag<cr>", { noremap = true, silent = true, desc = "ctag" })
+	map({ "n", "v" }, prefix .. "s", "<cmd>CsPrompt s<cr>", { desc = sym_map.s })
+	map({ "n", "v" }, prefix .. "g", "<cmd>CsPrompt g<cr>", { desc = sym_map.g })
+	map({ "n", "v" }, prefix .. "c", "<cmd>CsPrompt c<cr>", { desc = sym_map.c })
+	map({ "n", "v" }, prefix .. "t", "<cmd>CsPrompt t<cr>", { desc = sym_map.t })
+	map({ "n", "v" }, prefix .. "e", "<cmd>CsPrompt e<cr>", { desc = sym_map.e })
+	map({ "n", "v" }, prefix .. "f", "<cmd>CsPrompt f<cr>", { desc = sym_map.f })
+	map({ "n", "v" }, prefix .. "i", "<cmd>CsPrompt i<cr>", { desc = sym_map.i })
+	map({ "n", "v" }, prefix .. "d", "<cmd>CsPrompt d<cr>", { desc = sym_map.d })
+	map({ "n", "v" }, prefix .. "a", "<cmd>CsPrompt a<cr>", { desc = sym_map.a })
+	map({ "n", "v" }, prefix .. "b", "<cmd>Cs db build<cr>", { desc = sym_map.b })
+	map({ "n", "v" }, "<C-]>", "<cmd>Cstag<cr>", { noremap = true, silent = true, desc = "ctag" })
 end
 
 return M
